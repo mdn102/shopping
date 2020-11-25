2870b30d76ec79fdfc908745cebff3a6c3b032dc
nguyendm77 <nguyendm77@gmail.com>
2020-11-24 16:24:16 -0800

    make it better

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,13 @@ const IndexPage = () => {
           <Card>
             <Heading>Fresh</Heading>
             <Text variant={"text.default"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nisi mi, efficitur a dignissim vel, commodo vitae est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet lectus nec magna ornare tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc egestas auctor elit. Fusce id quam vitae ipsum sollicitudin sagittis. Maecenas faucibus venenatis nunc.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nisi mi, efficitur a dignissim vel, commodo vitae est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet lectus nec magna ornare tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
             </Text>
           </Card>
           <Card>
             <Heading>Tasty</Heading>
             <Text variant={"text.default"}>
-            Phasellus diam nulla, tempor eget elit nec, efficitur semper mi. Duis consequat ac nibh non vehicula. Suspendisse consectetur rutrum tellus, quis sollicitudin eros posuere quis. Cras vel placerat tortor, sit amet ullamcorper nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec a lobortis diam. Aenean sed sollicitudin turpis. Pellentesque suscipit quam sit amet lacus pretium placerat.
+            Phasellus diam nulla, tempor eget elit nec, efficitur semper mi. Duis consequat ac nibh non vehicula. Suspendisse consectetur rutrum tellus, quis sollicitudin eros posuere quis. Cras vel placerat tortor, sit amet ullamcorper nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
 
 
             </Text>
@@ -44,7 +44,7 @@ const IndexPage = () => {
           <Card>
             <Heading>Organic</Heading>
             <Text variant={"text.default"}>
-            Suspendisse accumsan tincidunt risus, vitae efficitur felis volutpat nec. Etiam eu blandit magna. Maecenas sed ex tortor. Pellentesque sit amet sodales leo. Morbi ac nisi volutpat, eleifend lorem a, volutpat felis. Aenean porta eu ex ac imperdiet. Ut sit amet dictum felis. Fusce in justo et nulla maximus eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+            Suspendisse accumsan tincidunt risus, vitae efficitur felis volutpat nec. Etiam eu blandit magna. Maecenas sed ex tortor. Pellentesque sit amet sodales leo. Morbi ac nisi volutpat, eleifend lorem a, volutpat felis. Aenean porta eu ex ac imperdiet. Ut sit amet dictum felis. Fusce in justo et nulla maximus eleifend.
             </Text>
           </Card>
         </Grid>
@@ -57,7 +57,7 @@ const IndexPage = () => {
             />
           </Box>
           <Box>
-            <Heading>This store is great</Heading>
+            <Heading>Organic Fruit Farm</Heading>
             <Text variant={"text.default"}>
             Nunc ultrices pharetra vehicula. Nunc convallis nisl eget augue scelerisque, eget cursus erat accumsan. Vestibulum in ultricies est, ac efficitur justo. Nunc elementum ex vitae elit facilisis, porta mollis ipsum laoreet. Integer eu elit egestas, tempus magna nec, sollicitudin risus. Donec consectetur tempus condimentum. Ut dignissim consequat hendrerit. Sed erat quam, porttitor in mollis sit amet, facilisis at ante. Nam finibus aliquam mi, eu commodo lectus faucibus a.
             </Text>
@@ -65,7 +65,7 @@ const IndexPage = () => {
         </Grid>
         <Grid columns={[1, 2, 2]}>
           <Box>
-            <Heading>This store is great</Heading>
+            <Heading>Fresh Pickup Everyday</Heading>
             <Text variant={"text.default"}>
             Fusce sed suscipit mauris. Nunc tincidunt, libero sed cursus tincidunt, quam erat pulvinar erat, quis luctus ipsum ex eu justo. Phasellus quis rutrum justo, nec pellentesque odio. Morbi posuere nibh et ante mattis, eu elementum augue sollicitudin. Nulla laoreet nunc eu orci varius, vel cursus ipsum convallis. Ut fringilla sit amet ante sed luctus. Sed est leo, pharetra ut viverra a, consectetur vel lacus. Sed luctus felis vitae gravida sagittis. Sed ultrices dolor nisi, ut aliquet libero eleifend eget. Aenean tempus neque ut urna blandit tincidunt.
             </Text>
